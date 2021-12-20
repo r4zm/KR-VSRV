50c206d7e0833ea08e3bebee3b51d5561d6af95a
r4zm <80853453+r4zm@users.noreply.github.com>
2021-12-20 03:48:43 +0300

    Update WirelessRobotPttp.py

--- WirelessRobotPttp.py
+++ WirelessRobotPttp.py
@@ -7,17 +7,17 @@ GPIO.setmode(GPIO.BOARD)
 
 motor1a = 7
 motor1b = 11
-#motor1e = 22
+motor1e = 22
 motor2a = 13
 motor2b = 16
-#motor2e = 15
+motor2e = 15
 
 GPIO.setup(motor1a,GPIO.OUT)
 GPIO.setup(motor1b,GPIO.OUT)
-#GPIO.setup(motor1e,GPIO.OUT)
+GPIO.setup(motor1e,GPIO.OUT)
 GPIO.setup(motor2a,GPIO.OUT)
 GPIO.setup(motor2b,GPIO.OUT)
-#GPIO.setup(motor2e,GPIO.OUT)
+GPIO.setup(motor2e,GPIO.OUT)
 
 
 screen = curses.initscr()
@@ -34,42 +34,42 @@ try:
             elif char == curses.KEY_UP:
                 GPIO.output(motor1a,GPIO.HIGH)
                 GPIO.output(motor1b,GPIO.LOW)
-                #GPIO.output(motor1e,GPIO.HIGH)
+                GPIO.output(motor1e,GPIO.HIGH)
                 GPIO.output(motor2a,GPIO.HIGH)
                 GPIO.output(motor2b,GPIO.LOW)
-                #GPIO.output(motor2e,GPIO.HIGH)
+                GPIO.output(motor2e,GPIO.HIGH)
                 print "move forward"
             elif char == curses.KEY_DOWN:
                 GPIO.output(motor1a,GPIO.LOW)
                 GPIO.output(motor1b,GPIO.HIGH)
-                #GPIO.output(motor1e,GPIO.HIGH)
+                GPIO.output(motor1e,GPIO.HIGH)
                 GPIO.output(motor2a,GPIO.LOW)
                 GPIO.output(motor2b,GPIO.HIGH)
-                #GPIO.output(motor2e,GPIO.HIGH)
+                GPIO.output(motor2e,GPIO.HIGH)
                 print "move backward"
             elif char == curses.KEY_RIGHT:
                 GPIO.output(motor1a,GPIO.HIGH)
                 GPIO.output(motor1b,GPIO.LOW)
-                #GPIO.output(motor1e,GPIO.HIGH)
+                GPIO.output(motor1e,GPIO.HIGH)
                 GPIO.output(motor2a,GPIO.LOW)
                 GPIO.output(motor2b,GPIO.HIGH)
-                #GPIO.output(motor2e,GPIO.HIGH)
+                GPIO.output(motor2e,GPIO.HIGH)
                 print "turn right"
             elif char == curses.KEY_LEFT:
                 GPIO.output(motor1a,GPIO.LOW)
                 GPIO.output(motor1b,GPIO.HIGH)
-                #GPIO.output(motor1e,GPIO.HIGH)
+                GPIO.output(motor1e,GPIO.HIGH)
                 GPIO.output(motor2a,GPIO.HIGH)
                 GPIO.output(motor2b,GPIO.LOW)
-                #GPIO.output(motor2e,GPIO.HIGH)
+                GPIO.output(motor2e,GPIO.HIGH)
                 print "turn left"
             elif char == 10:
                 GPIO.output(motor1a,GPIO.LOW)
                 GPIO.output(motor1b,GPIO.LOW)
-                #GPIO.output(motor1e,GPIO.LOW)
+                GPIO.output(motor1e,GPIO.LOW)
                 GPIO.output(motor2a,GPIO.LOW)
                 GPIO.output(motor2b,GPIO.LOW)
-                #GPIO.output(motor2e,GPIO.LOW)
+                GPIO.output(motor2e,GPIO.LOW)
                 print "stop"
   
              
